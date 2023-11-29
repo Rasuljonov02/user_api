e3bd3160f9c6866f3715522c1ce4799e49912b78
Rasuljonov <Rasuljonov@Muhammad>
2023-11-29 20:35:45 +0500

    4554

--- src/index.ts
+++ src/index.ts
@@ -3,70 +3,87 @@ import axios from "axios";
 
 const URL = "https://jsonplaceholder.typicode.com/users";
 const contener: HTMLDListElement = document.querySelector(".con")!;
-const a: any[] = [];
+let a: any[] = [];
 
-async function searchVideos() {
-    try {
-        const response = await axios.get(`${URL}`);
-        a.push(response.data);
-        console.log(a);
-        UserMalumot(a[0]);
-    } catch (error) {
-        console.error(error);
-    }
+async function searchUser() {
+	try {
+		const response = await axios.get(`${URL}`);
+
+		a.push(response.data);
+console.log(a);
+
+UserMalumot(a[0]);
+
+	} catch (error) {
+		console.error(error);
+	}
 }
 
+
+
+
 function UserMalumot(data: any[]) {
-    for (let i = 0; i < data.length; i++) {
-        const ele = data[i];
+	for (let i = 0; i < data.length; i++) {
+		const ele = data[i];
 
-        const tr: HTMLTableRowElement = document.createElement("tr");
-        const name: HTMLTableCellElement = document.createElement("td");
-        const email: HTMLTableCellElement = document.createElement("td");
-        const tel: HTMLTableCellElement = document.createElement("td");
-        const id: HTMLTableCellElement = document.createElement("td");
-        const imgg: HTMLImageElement = document.createElement("img");
-        const imgg2: HTMLImageElement = document.createElement("img");
-        const span: HTMLSpanElement = document.createElement("span");
-        tr.id = `row-${i + 1}`;
-        tr.classList.add("data-row"); 
-        name.innerText = `${ele.name}`;
-        name.className = "nametd";
-        id.innerText = `${i + 1}`;
-        id.className = "idtd";
+		const tr: HTMLTableRowElement = document.createElement("tr");
+		const name: HTMLTableCellElement = document.createElement("td");
+		const email: HTMLTableCellElement = document.createElement("td");
+		const tel: HTMLTableCellElement = document.createElement("td");
+		const id: HTMLTableCellElement = document.createElement("td");
+		const imgg: HTMLImageElement = document.createElement("img");
+		const imgg2: HTMLImageElement = document.createElement("img");
+		// const span: HTMLSpanElement = document.createElement("span");
+		tr.id = `row-${i + 1}`;
+		tr.classList.add("data-row");
+		name.innerText = `${ele.name}`;
+		name.className = "nametd";
+		id.innerText = `${i + 1}`;
+		id.className = "idtd";
 
-        email.innerText = `${ele.email}`;
-        email.className = "emailtd";
+		email.innerText = `${ele.email}`;
+		email.className = "emailtd";
 
-        tel.innerText = `${ele.phone}`;
-        tel.className = "teltd";
-        imgg.src = "https://www.freeiconspng.com/thumbs/eye-icon/eyeball-icon-png-eye-icon-1.png";
-        imgg.className = "img";
-        imgg2.src = "https://cdn-icons-png.flaticon.com/512/1345/1345874.png";
-        imgg2.className = "img1";
+		tel.innerText = `${ele.phone}`;
+		tel.className = "teltd";
+		imgg.src = "https://www.freeiconspng.com/thumbs/eye-icon/eyeball-icon-png-eye-icon-1.png";
+		imgg.className = "img";
+		imgg2.src = "https://cdn-icons-png.flaticon.com/512/1345/1345874.png";
+		imgg2.className = "img1";
 
-        contener.appendChild(tr);
-        tr.appendChild(id);
-        tr.appendChild(name);
-        tr.appendChild(email);
-        tr.appendChild(tel);
-        tr.appendChild(span);
+		contener.appendChild(tr);
+		tr.appendChild(id);
+		tr.appendChild(name);
+		tr.appendChild(email);
+		tr.appendChild(tel);
+		// tr.appendChild(span);
 
-        span.appendChild(imgg);
-        span.appendChild(imgg2);
-    }
-    remov();
+		tr.appendChild(imgg);
+		tr.appendChild(imgg2);
+	}
+	remov();
 }
 
 function remov() {
-    const rows: NodeListOf<HTMLTableRowElement> = document.querySelectorAll(".data-row")!;
-    console.log(rows);
+	const rows: NodeListOf<HTMLImageElement> = document.querySelectorAll(".img1")!;
+	console.log(rows);
 
-    rows.forEach((row) => {
-        row.addEventListener("click", () => {
-            row.remove();
-        });
-    });
+	rows.forEach((rowi) => {
+					rowi.addEventListener("click", () => {
+									// Remove the parent element of the clicked image
+									const parentRow = rowi.parentElement;
+									if (parentRow && parentRow.nodeType === Node.ELEMENT_NODE) {
+													parentRow.remove();
+													// If needed, call qaytaChizish here
+													// qaytaChizish();
+									}
+					});
+	});
 }
 
-searchVideos();
+function qaytaChizish() {
+	contener.innerHTML = "";
+	UserMalumot(a);
+}
+
+searchUser();
